58b4186d3dbc20aabfcebe7f765e3cbd46f65526
iosdevvivek <iosdev.vivek@gmail.com>
2015-09-07 18:12:22 +0530

    Create NSFILEMANAGER.m

---
+++ NSFILEMANAGER.m
@@ -0,0 +1,4 @@
+NSFileManager *fileManager = [NSFileManager defaultManager];
+NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+NSString *filePath = [documentsPath stringByAppendingPathComponent:@"file.txt"];
+BOOL fileExists = [fileManager fileExistsAtPath:filePath];
