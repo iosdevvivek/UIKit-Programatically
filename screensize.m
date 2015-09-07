bd38054afabbb7448923a3b02576bcaa959abcf6
iosdevvivek <iosdev.vivek@gmail.com>
2015-09-07 17:36:11 +0530

    Update screensize.m

--- screensize.m
+++ screensize.m
@@ -1,3 +1,10 @@
 CGRect screenRect = [[UIScreen mainScreen] bounds];
 CGFloat screenWidth = screenRect.size.width;
 CGFloat screenHeight = screenRect.size.height;
+
+
+//
+CGRect screenBound = [[UIScreen mainScreen] bounds];
+CGSize screenSize = screenBound.size;  
+CGFloat screenWidth = screenSize.width;
+CGFloat screenHeight = screenSize.height;
